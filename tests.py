965ae6cde255357a8e66e9f85a46d793a12baf9e
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-10-01 13:58:13 -0400

    fixing test tolerances:

--- tests.py
+++ tests.py
@@ -6,14 +6,15 @@ from frames import frames
 def test_trpy_zeros():
     matrix = frames.trpy_evolving(0, 0, 0, 0, 0, 0)
     assert np.allclose(matrix, np.array([[1, 0, 0, 0],
-                                            [0, 1, 0, 0],
-                                            [0, 0, 1, 0],
-                                            [0, 0, 0, 1]]))
+                                         [0, 1, 0, 0],
+                                         [0, 0, 1, 0],
+                                         [0, 0, 0, 1]]))
 
 
 def test_trpy_ones():
     matrix = frames.trpy_evolving(1, 1, 1, 1, 1, 1)
     assert np.allclose(matrix, np.array([[0.2919, -0.0721, 0.9537, 1],
-                                            [0.4546, 1.0504, -0.0721, 1],
-                                            [-0.8515, 0.4546, 0.2919, 1],
-                                            [0, 0, 0, 1]]))
+                                         [0.4546, 0.8877, -0.0721, 1],
+                                         [-0.8414, 0.4546, 0.2919, 1],
+                                         [0, 0, 0, 1]]),
+                       rtol=1.e-5, atol=1.e-4)
