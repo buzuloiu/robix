03ce76aa2c07c3e6b5bfb3cf40692307bb7062be
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-10-15 14:36:16 -0400

    a matrix

--- frames/frames.py
+++ frames/frames.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def trpy_evolving(t_x, t_y, t_z, r_z, r_y, r_x):
+
+def _compute_trpy_evolving(t_x, t_y, t_z, r_z, r_y, r_x):
     return np.array([[np.cos(r_z)*np.cos(r_y),
                       ((np.cos(r_z)*np.sin(r_y)*np.sin(r_x))-(np.sin(r_z)*np.cos(r_x))),
                       ((np.cos(r_z)*np.sin(r_y)*np.cos(r_x))+(np.sin(r_z)*np.sin(r_x))),
@@ -12,4 +13,22 @@ def trpy_evolving(t_x, t_y, t_z, r_z, r_y, r_x):
                      [-1*np.sin(r_y),
                       np.cos(r_y)*np.sin(r_x),
                       np.cos(r_y)*np.cos(r_x), t_z],
+                     [0, 0, 0, 1]])
+
+
+def _compute_a_matrix(l, d, alpha, theta):
+    alpha = np.deg2rad(alpha)
+    theta = np.deg2rad(theta)
+    return np.array([[np.cos(theta),
+                      (-1*np.sin(theta)*np.cos(alpha)),
+                      (np.sin(theta)*np.cos(alpha)),
+                      (l*np.cos(theta))],
+                     [np.sin(theta),
+                      np.cos(theta)*np.cos(alpha),
+                      (-1*np.cos(theta)*np.sin(alpha)),
+                      l*np.sin(theta)],
+                     [0,
+                      np.sin(alpha),
+                      np.cos(alpha),
+                      d],
                      [0, 0, 0, 1]])
--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ from frames import frames
 
 
 def test_trpy_zeros():
-    matrix = frames.trpy_evolving(0, 0, 0, 0, 0, 0)
+    matrix = frames._compute_trpy_evolving(0, 0, 0, 0, 0, 0)
     assert np.allclose(matrix, np.array([[1, 0, 0, 0],
                                          [0, 1, 0, 0],
                                          [0, 0, 1, 0],
@@ -12,9 +12,18 @@ def test_trpy_zeros():
 
 
 def test_trpy_ones():
-    matrix = frames.trpy_evolving(1, 1, 1, 1, 1, 1)
+    matrix = frames._compute_trpy_evolving(1, 1, 1, 1, 1, 1)
     assert np.allclose(matrix, np.array([[0.2919, -0.0721, 0.9537, 1],
                                          [0.4546, 0.8877, -0.0721, 1],
                                          [-0.8414, 0.4546, 0.2919, 1],
+                                         [0, 0, 0, 1]]),
+                       rtol=1.e-5, atol=1.e-4)
+
+
+def test_a_matrix_zeros():
+    matrix = frames._compute_a_matrix(0, 0, 0, 0)
+    assert np.allclose(matrix, np.array([[1, 0, 0, 0],
+                                         [0, 1, 0, 0],
+                                         [0, 0, 1, 0],
                                          [0, 0, 0, 1]]),
                        rtol=1.e-5, atol=1.e-4)
