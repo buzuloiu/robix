4e925eeae7c9d32e7f95e4c2cfb9a2aea3c5dc6f
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-10-29 14:46:52 -0400

    printing command strings

--- frames/config.py
+++ frames/config.py
@@ -1,17 +1,17 @@
 global config
 
 config = {
-    'theta1': {'min': -80.0, 'max': 80.0, 'offset': 95.7, 'l': 9.28, 'd': 16.39, 'alpha':  0.0, 'theta_offset': 0, 'theta_sign':-1},
-    'theta2': {'min': 0,     'max':    1, 'offset':    1, 'l': 9.15, 'd':  0, 'alpha': 90.0, 'theta_offset': 0, 'theta_sign':-1},
-    'theta3': {'min': 0,     'max':    1, 'offset':    1, 'l': 5.74, 'd':   0.0, 'alpha':  0.0, 'theta_offset': 0, 'theta_sign':-1},
-    'theta4': {'min': 0,     'max':    1, 'offset':    1, 'l':  0.0, 'd': 0, 'alpha': 90.0, 'theta_offset': 90, 'theta_sign':1},
-    'theta5': {'min': 0,     'max':    1, 'offset':    1, 'l':  0.0, 'd':  10, 'alpha':  0.0, 'theta_offset': 10, 'theta_sign':-1}
+    'theta1': {'min': -80.0, 'max': 80.0, 'offset': 0, 'l': 9.28, 'd': 16.39, 'alpha':  0.0, 'theta_offset': 0, 'theta_sign':-1},
+    'theta2': {'min': -80.0, 'max': 75.0, 'offset': 0, 'l': 9.15, 'd':  0, 'alpha': 90.0, 'theta_offset': 0, 'theta_sign':-1},
+    'theta3': {'min': -80.0, 'max': 85.0, 'offset': 0, 'l': 5.74, 'd':   0.0, 'alpha':  0.0, 'theta_offset': 0, 'theta_sign':-1},
+    'theta4': {'min': -85.0, 'max': 85.0, 'offset': 0, 'l':  0.0, 'd': 0, 'alpha': 90.0, 'theta_offset': 90, 'theta_sign':1},
+    'theta5': {'min': -75.0, 'max': 75.0, 'offset': 0, 'l':  0.0, 'd':  10, 'alpha':  0.0, 'theta_offset': 10, 'theta_sign':-1}
 }
 
 thetas = [
-    ['theta1', 90],
-    ['theta2', -90],
-    ['theta3',-45],
-    ['theta4', -80],
-    ['theta5', 0]
+    ['theta1', 45],
+    ['theta2', 30],
+    ['theta3', -25],
+    ['theta4', -60],
+    ['theta5', 15]
 ]
--- robix.py
+++ robix.py
@@ -4,20 +4,21 @@ import numpy as np
 import sys
 
 
-def generate_robix_command(theta1, theta2, theta3, theta4, theta5, theta6):
-    return "MOVE 1 TO {}, 2 TO {}, 3 TO {}, 4 TO {}, 5 TO {}, 6 TO {}".format(
+def generate_robix_command(theta1, theta2, theta3, theta4, theta5):
+    return "MOVE 1 TO {}, 2 TO {}, 3 TO {}, 4 TO {}, 5 TO {}".format(
         convert_degrees_to_robix('theta1', theta1),
         convert_degrees_to_robix('theta2', theta2),
         convert_degrees_to_robix('theta3', theta3),
         convert_degrees_to_robix('theta4', theta4),
         convert_degrees_to_robix('theta5', theta5),
-        convert_degrees_to_robix('theta6', theta6),
     )
 
 
 def convert_degrees_to_robix(name, degrees):
+    if degrees < config[name]['min'] and degrees > config[name]['max']:
+        raise Exception('Robix motor "{}" is out of range ({}, {})'.format(name, config[name]['min'], config[name]['max']))
     return int(
-        ((config[name]['max'] - config[name]['min']) / 2800.)*degrees + config[name]['offset']
+        (2800./(config[name]['max'] - config[name]['min']))*degrees + config[name]['offset']
     )
 
 
@@ -41,6 +42,13 @@ def apply_fwd_kin(x=0, y=0, z=0):
 
 
 if __name__ == "__main__":
+    print(generate_robix_command(
+        thetas[0][1], #config['theta1']['theta_sign'],
+        thetas[1][1], #config['theta2']['theta_sign'],
+        thetas[2][1], #config['theta3']['theta_sign'],
+        thetas[3][1], #config['theta4']['theta_sign'],
+        thetas[4][1], #config['theta5']['theta_sign']
+    ))
     if len(sys.argv) == 4:
         x, y, z = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
         print(apply_fwd_kin(x, y, z))
