0daa75dc8c219d389ddc735c0e6b3a34e65cc52c
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-11-14 13:07:58 -0500

    math

--- symbolic_math/symbolic_math.py
+++ symbolic_math/symbolic_math.py
@@ -1,6 +1,10 @@
 from sympy import Symbol, cos, sin
+from sympy import *
 from sympy.printing.latex import print_latex
-from sympy.matrices import Matrix
+from sympy.printing import latex
+from sympy.matrices import Matrix, eye
+
+
 
 t_1 = Symbol('theta_1')
 t_2 = Symbol('theta_2')
@@ -20,8 +24,29 @@ l_3 = Symbol('l_3')
 l_4 = Symbol('l_4')
 l_5 = Symbol('l_5')
 
-a_1 = Matrix([])
+a_1 = Symbol('alpha_1')
+a_2 = Symbol('alpha_2')
+a_3 = Symbol('alpha_3')
+a_4 = Symbol('alpha_4')
+a_5 = Symbol('alpha_5')
 
 
-e = 1/cos(t_1)
-print_latex(e.series(t_1, 0, 10))
+def a_matrix(theta, alpha, l, d):
+    return Matrix([[cos(theta), -1*(sin(theta)*cos(alpha)), sin(theta)*sin(alpha), l*cos(theta)],
+                   [sin(theta), cos(theta)*cos(alpha), -1*cos(theta)*sin(alpha), l*sin(theta)],
+                   [0, sin(alpha), cos(alpha), d],
+                   [0, 0, 0, 1]])
+
+
+output = eye(4)
+
+a_matrices = [a_matrix(t_1, a_1, l_1, d_1),
+              a_matrix(t_2, a_2, l_2, d_2)
+              a_matrix(t_3, a_3, l_3, d_3),
+              a_matrix(t_4, a_4, l_4, d_4),
+              a_matrix(t_5, a_5, l_5, d_5)]
+
+for matrix in a_matrices:
+    output = output*matrix
+
+print(latex((output)))
