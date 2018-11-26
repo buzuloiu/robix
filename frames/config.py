5708b26cc956838e5d2a9f2b869648dd2968bc9f
Nikhil <nperi104@uottawa.ca>
2018-11-26 13:33:58 -0500

    I grab the block

--- frames/config.py
+++ frames/config.py
@@ -18,10 +18,10 @@ thetas = [
 # circle: diameter 2.5cm
 # triangle: side 2.7cm
 # square: side 2.2cm
-#rectangle: sideA 1.9cm sideB 3.6cm 
+#rectangle: sideA 1.9cm sideB 3.6cm
 blocks = {
-    'circle': {'open': , 'close':}
-    'triangle':
-    'square':
-    'reactangle':
+    'circle': {'open': -1400, 'close': -443}
+    'triangle' {'open':-1400, 'close': -409}
+    'square' {'open': -1400, 'close': -217}:
+    'reactangle' {'open': -1400, 'close': -200}:
 }
