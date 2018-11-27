015121be62d03b3f7f7b546b7c8eca58fc100ab7
Nikhil <nperi104@uottawa.ca>
2018-11-27 15:14:32 -0500

    inital

--- camera/object_detector.py
+++ camera/object_detector.py
@@ -1,5 +1,57 @@
 import cv2
+import numpy as np
+import imutils
 
-from camera.stream import stream
+
+class ShapeDetector:
+	def __init__(self):
+		pass
+
+	def detect(self, c):
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+		if len(approx) == 3:
+			shape = "triangle"
+		elif len(approx) == 4:
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+		elif len(approx) == 5:
+			shape = "pentagon"
+		else:
+			shape = "circle"
+		return shape
+
+image = np.load('images/sample-1.npy')
+gray = cv2.cvtColor(image[0:150, 0:300], cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+# find contours in the thresholded image and initialize the
+# shape detector
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+sd = ShapeDetector()
 
 import pdb; pdb.set_trace()
+for c in cnts:
+	# compute the center of the contour, then detect the name of the
+	# shape using only the contour
+	M = cv2.moments(c)
+	cX = int((M["m10"] / M["m00"]))
+	cY = int((M["m01"] / M["m00"]))
+	shape = sd.detect(c)
+
+	# multiply the contour (x, y)-coordinates by the resize ratio,
+	# then draw the contours and the name of the shape on the image
+	c = c.astype("int")
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+		0.5, (255, 255, 255), 2)
+
+	# show the output image
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
